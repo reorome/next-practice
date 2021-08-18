62a0678c2ec7bed9fd6bdf22e6b242a12240a5be
reorome <with.ot24@gmail.com>
2021-08-18 21:11:00 +0000

    React.js公式チュートリアル　三目ならべ完成

--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,38 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import { getPracticeData } from '../lib/practice'
 import Link from 'next/link'
 import Date from '../components/date'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
+  const allPracticeData = getPracticeData()
   return {
     props: {
-      allPostsData
+      allPostsData,
+      allPracticeData
     }
   }
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({ allPostsData, allPracticeData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>サンプルページ</h2>
+        <ul>
+          {allPracticeData.map(({id}) =>(
+            <li className={utilStyles.listItem} key={id}>
+            <Link href={`/practice/${id}`}>
+              <a>{id}</a>
+            </Link>
+          </li>
+          ))}
+        </ul>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
